682215557f8b3a6352fcf9cbc1f40b7d92c82f49
Tahir-47 <tahirmohd909@gmail.com>
2022-07-23 20:09:39 +0530

    MIgration to PostgreSQL

--- miniproject/miniproject/settings.py
+++ miniproject/miniproject/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'miniproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'meshub',
+        'USER': 'postgres',
+        'PASSWORD': 'qet135',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
