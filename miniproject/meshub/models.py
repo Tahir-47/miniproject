c5a8c12ad182508de6d5013cff628930cbe78678
tahir <81474461+Tahir-47@users.noreply.github.com>
2022-07-28 12:51:20 +0530

    User and admin privileges

--- miniproject/meshub/models.py
+++ miniproject/meshub/models.py
@@ -10,6 +10,7 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
+    isAdmin = models.BooleanField(default=False)
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
